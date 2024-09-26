af4c4684869ec7746ea08b2e372dc68feb21cbfd
carlos-ex-santos <carlos_exs@hotmail.com>
2024-09-25 21:01:24 -0300

    Alterado a configuração para deixar dinâmica com o parâmetro do .end

--- backend/conf/database.js
+++ backend/conf/database.js
@@ -3,7 +3,7 @@ import { Sequelize } from 'sequelize'
 
 const db = new Sequelize(process.env.DB_BASE, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    dialect: "mysql"
+    dialect: process.env.DB_DIALECT
 })
 
 export default db
